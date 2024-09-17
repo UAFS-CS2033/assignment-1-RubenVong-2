aec3dab31861ba552de460b9d360ceb6b20ee9c0
Ruben Vongsasombath <rvongs00@uafs.edu>
2024-09-17 13:53:41 -0500

    last

--- Server.java
+++ Server.java
@@ -2,9 +2,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.Reader;
 import java.net.ServerSocket;
 import java.net.Socket;
-//import java.net.URL;
+//import java.io.FileReader;
 
 public class Server{
     private ServerSocket serverSocket;
@@ -18,29 +19,32 @@ public class Server{
     private void processConnection() throws IOException{
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
+        //FileReader fr = new FileReader("assignment-1-RubenVong-2/docroot/home.html");
 
         //*** Application Protocol *****
         String buffer = in.readLine();
-        while(buffer.length()!=0){
+        String[] getRequest = buffer.split("[/ ]");
+        /*while(buffer.length()!=0){
             System.out.println("Client: " + buffer);
             buffer = in.readLine();
-        }
-        //URL url = new URL("docroot/home.html");
-        //BufferedReader in = new BufferedReader(new InputStreamReader(null, "docroot/home.html"));
-        
-        /*String buffer = in.readLine();
-        while(buffer != null){
-            System.out.println(buffer);
-            //buffer = in.readLine();
         }*/
-
+        
         out.printf("HTTP/1.1 200 OK\n");
         out.printf("Content-Length: 34\n");
         out.printf("Content-Type: text/html\n\n");
 
-        out.printf("<h1>welcome to the web Server</h1>");
-        //out.printf("<link rel=\"docroot/home.html\"");
-        
+        if (getRequest[2].equals("home.html")) {
+            out.printf("<h1>welcome to the web Server</h1>");
+            String file = "assignment-1-RubenVong-2/docroot/home.html";
+            //BufferedReader br = new BufferedReader(file);
+            //String val;
+            //while ((val = br.readLine()) != null) {
+                //System.out.println(val);
+            //}
+            //br.close();
+        } else {
+            out.printf("<h1>this is not home</h1>");
+        }
         in.close();
         out.close();
     }
